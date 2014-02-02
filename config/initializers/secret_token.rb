f6d3b1c4c6efef58a05901b18853de8052ca0c3a
CentOS-6.3-x86_64 <harada@localhost.localdomain>
2014-02-02 23:19:43 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ad7750c993bc2e1403a188e8f74eb82390c6dfd3f7922a81fcd88ba6a56631a1a6e70276d53211c90cf87587be1c566d4f17e5a4b136cbf6131030fc417863b2'
